7b486f837e8f43b1f4180791c724a7a81e49cc9d
Daniel <daniel.sergun@hotmail.com>
2025-12-18 14:21:25 +0200

    main page view updates

---
+++ app/webApp.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/webApp.html');
+});
+
+
+
+
+
+
+app.use(express.static(__dirname));
+
+app.listen(port, () => {
+    console.log(`server running on  ${port}`);
+});
+
