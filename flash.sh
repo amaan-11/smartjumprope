b600716c8fa885ff96ed9ba07e4dbcbcd6b20274
Nischal <nischaga@metropolia.fi>
2025-12-19 11:58:59 +0200

    edited flash

--- flash.sh
+++ flash.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-
 cd 
 . ~/esp/esp-idf/export.sh
 cd ~/smartjumprope
-idf.py set-target esp32c6    
-idf.py fullclean
 idf.py build
 idf.py -p /dev/ttyACM0 flash monitor
 #use ctrl+] to exit monitoring
