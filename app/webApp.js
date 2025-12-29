38a811a5f49722e26d048a4873afd668e050245e
Daniel <daniel.sergun@hotmail.com>
2025-12-29 15:24:36 +0200

    app input added

--- app/webApp.js
+++ app/webApp.js
@@ -2,17 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static(__dirname));
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/webApp.html');
 });
 
 
+app.post('/login', (req, res) => {
+    const { username, user_id } = req.body;
+    console.log(username, user_id);
 
+    res.send('<h1>Welcome!</h1>');
+});
 
-
-
-app.use(express.static(__dirname));
 
 app.listen(port, () => {
     console.log(`server running on  ${port}`);
