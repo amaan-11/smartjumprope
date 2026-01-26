e1261059475d9b96cb3203646b398b51b4642941
Daniel <daniel.sergun@hotmail.com>
2026-01-26 12:06:05 +0200

    login authentication

--- app/private/webApp.js
+++ app/private/webApp.js
@@ -3,9 +3,19 @@ const app = express();
 const path = require('path');
 const port = 3000;
 
+const session = require('express-session');
+
 app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 
+const temp_key = "fredvccxv4535dfs";
+
+app.use(session({
+  secret: temp_key,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { secure: false }
+}));
 
 //If .html not index!
 /*
@@ -21,6 +31,9 @@ app.post('/login', (req, res) => {
     const n = "daniel";
     const u = "2";
     if (username === n && user_id === u) {
+
+        req.session.user = { username: n };
+
         res.redirect('/user-data/rope');
     }
     else {
@@ -30,9 +43,26 @@ app.post('/login', (req, res) => {
     //res.sendFile(__dirname + '/data.html');
 });
 
+
 app.get('/user-data/rope', (req, res) => {
 
     res.sendFile(path.join(__dirname, '../public/data.html'));
+});
+
+
+function authMiddleware(req, res, next) {
+  if (!req.session || !req.session.user) {
+    return res.status(401).json({ error: "Not authorized" });
+  }
+  next();
+}
+
+app.get('/api/me', authMiddleware, (req, res) => {
+  const user = req.session.user;
+
+  res.json({
+    username: user.username,
+  });
 });
 
 
